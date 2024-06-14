f1814824ba5d26b7e939d9356d4d17bf8a74b6af
Apple <apple@Rajats-mac.local>
2024-06-14 15:30:02 +0530

    Added default home route and message for incorrect route

--- app.js
+++ app.js
@@ -6,6 +6,13 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.get("/", (req, res) => {
+  res.status(200).send("Welcome to api");
+});
 app.use("/api/v1/users", userRouter);
+
+app.use((req, res) => {
+  res.status(404).send("Error:Route not found");
+});
 
 module.exports = app;
